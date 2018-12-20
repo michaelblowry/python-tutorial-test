14a32d2fca05fbeccc01dfa0fe7eb8320ed3e1a1
michaelblowry <43074714+michaelblowry@users.noreply.github.com>
2018-12-20 15:08:49 -0800

    New example py

--- example1.py
+++ example1.py
@@ -10,16 +10,17 @@ def function1(arg1, arg2):
     arg1 = arg2
     return arg1
 
+
 def function2(arg1, arg2):
     """
     This function has google style.
+
     Args:
         arg1:
         arg2:
 
     Returns:
         out: the output
-
     """
     arg1 = arg2
     out = arg2
---
+++ sphinx/If_statements.rst
@@ -0,0 +1,10 @@
+If Statement examples
+=====================
+
+Section 1
+-----------
+I will do these things:
+1 Hello
+2 Goodbye
+
+
