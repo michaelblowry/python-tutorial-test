dd7a3e8abaed568d703f31c92ad5f368af121a5c
michaelblowry <43074714+michaelblowry@users.noreply.github.com>
2018-12-20 11:36:00 -0800

    New example py

---
+++ example1.py
@@ -0,0 +1,26 @@
+def function1(arg1, arg2):
+    """
+    This function takes two arguments and does math.
+
+    :param arg1: some value
+    :param arg2: another value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+def function2(arg1, arg2):
+    """
+    This function has google style.
+    Args:
+        arg1:
+        arg2:
+
+    Returns:
+        out: the output
+
+    """
+    arg1 = arg2
+    out = arg2
+    return out
